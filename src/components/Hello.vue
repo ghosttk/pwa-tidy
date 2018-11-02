4559871549cdc4dc5acf72c51cca5fafcbf32298
ghosttk <ghosttk@hotmail.com>
2018-11-02 11:53:56 +0800

    add Modal.vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="hello">
+    <modal @onCloseDialog="closeThis('isShowLog')" :is-show='isShowLog'></modal>
+    <Modal :show='mShow'></Modal>
     <div v-for="pi in tdata"> {{ pi.place }} 
+    <button @click="onShowAddPlace">AddPlace</button>
       <button v-for="item in pi.items"> {{ item }} </button>
     </div>
   </div>
 </template>
 
 <script>
+import modal from './dialog'
+import Modal from './Modal'
 export default {
   name: 'hello',
+  components: {
+    modal,
+    Modal
+  },
   data () {
     return {
-      tdata: ''
+      tdata: '',
+      isShowLog: false,
+      mShow: false
     }
   },
   mounted: function () {
     var mydate = new Date()
     this.tdata = [{place: '01', items: ['01'], dates: [mydate.toLocaleString()], unsaved: [true]}]
+  },
+  methods: {
+    onShowAddPlace: function () {
+      this.mShow = true
+    },
+    onCloseDialog: function () {
+      this.isShowLog = false
+    },
+    closeThis: function (dlg) {
+      console.log(dlg)
+      this.isShowLog = false
+    }
   }
 }
 </script>
